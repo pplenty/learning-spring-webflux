caf7d2867ae8ee4381e18658d2ce310b81a9aab1
jade <jade.koh@kakaocorp.com>
2022-07-17 18:01:50 +0900

    init commit

---
+++ src/main/kotlin/com/jade/learning/LearningSpringWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.jade.learning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningSpringWebfluxApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearningSpringWebfluxApplication>(*args)
+}
---
+++ src/test/kotlin/com/jade/learning/LearningSpringWebfluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jade.learning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningSpringWebfluxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
